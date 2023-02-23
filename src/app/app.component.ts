411dc4bee083cd050f59b3e84bb7d66fe648071a
amarkevych <aw.markevich@gmail.com>
2023-02-23 13:30:52 +0200

    Add initial calculation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'leverage-calculator';
+  investForm: FormGroup;
+  moneyInDeal: number = 0;
+
+  constructor() {
+    this.investForm = new FormGroup({
+      deposit: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$')]),
+      stopLossInPercents: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$')]),
+      entryPrice: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$')]),
+      stopLossPrice: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$')]),
+      leverage: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$')]),
+    });
+  }
+
+  calculateData(): void {
+    const movement = Number(this.investForm.value.entryPrice) > Number(this.investForm.value.stopLossPrice)
+      ? Number(((1 - (Number(this.investForm.value.stopLossPrice) / Number(this.investForm.value.entryPrice))) * 100).toFixed(2)) // long
+      : Number((((Number(this.investForm.value.stopLossPrice) / Number(this.investForm.value.entryPrice)) - 1) * 100).toFixed(2)); // short
+
+    this.moneyInDeal =
+      Number((Number(this.investForm.value.deposit)
+        * Number(this.investForm.value.stopLossInPercents) / 100
+        * (100 / (movement * Number(this.investForm.value.leverage)))).toFixed(2));
+  }
+
+  onSubmit(): void {
+    this.calculateData()
+  }
+
 }
